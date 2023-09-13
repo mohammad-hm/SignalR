e4062e8094856b8635c247eb1727d89b790c4959
mohammad hajimohammadi <hajimohammadi@amnpardaz.com>
2023-09-13 08:52:39 +0330

    create ValueController

---
+++ SignalR/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Value> GetValues()
+        {
+            return new Value() { id = 1, valueName = "valueName" };
+        }
+    }
+
+    public class Value
+    {
+        public int id { get; set; }
+        public string? valueName { get; set; }
+
+    }
+}
